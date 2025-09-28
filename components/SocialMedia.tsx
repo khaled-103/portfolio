c91adc47a7d568ed5e908f54f83ef549a25a4d3d
khaled Shiekh Ali <20khaled2001@gmail.com>
2025-09-28 13:30:38 +0300

    edit social media links

--- components/SocialMedia.tsx
+++ components/SocialMedia.tsx
@@ -4,7 +4,7 @@ import { FaGithub, FaLinkedinIn, FaWhatsapp } from "react-icons/fa";
 export default function SocialMedia() {
   const socialMedia = [
     { icon: FaGithub, desc: "GitHub", link: "https://github.com/khaled-103", color: "text-gray-900 dark:text-white" },
-    { icon: FaLinkedinIn, desc: "LinkedIn", link: "https://www.linkedin.com/in/khaled-shaikh-ali-5b9351249", color: "text-[#0077B5]" },
+    { icon: FaLinkedinIn, desc: "LinkedIn", link: "https://www.linkedin.com/in/khaled-shaikh-ali", color: "text-[#0077B5]" },
     // { icon: FaFacebookF, desc: "Facebook", link: "https://facebook.com", color: "text-[#1877F2]" },
     // { icon: FaInstagram, desc: "Instagram", link: "https://instagram.com", color: "text-pink-500" }, // أو متدرج (أشرح تحت 👇)
     { icon: FaWhatsapp, desc: "Whatsapp", link: "https://wa.me/972598210015", color: "text-[#25D366]" }
